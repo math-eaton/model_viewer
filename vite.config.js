b70d3016cd4621adb3fce259f2c3221103965e56
math-eaton <mheaton@ciesin.columbia.edu>
2024-09-03 23:26:11 -0400

    gh pages config

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+// import path from 'path';
+
+export default defineConfig({
+  base: '/model_viewer/',
+  build: {
+    rollupOptions: {
+      // output: {
+      //   manualChunks: {
+      //     'three': ['three'],
+      //     'tone': ['tone'],
+      //   }
+      // }
+    },
+    chunkSizeWarningLimit: 1000,
+  },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:8080',
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //   },
+  // },
+});
